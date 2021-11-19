bbe9bff6e1844c5e7d4345b44f5e00a98a64d4e7
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2021-11-19 14:58:18 +0630

    splash screen implementation with old method, before Android 12

--- app/src/main/java/com/galaxytechno/coresplashscreensample/MainActivity.kt
+++ app/src/main/java/com/galaxytechno/coresplashscreensample/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.galaxytechno.coresplashscreensample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Handle the splash screen transition.
         installSplashScreen()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 200L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
+            }
+        }
+
+
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/galaxytechno/coresplashscreensample/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.galaxytechno.coresplashscreensample
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
+    }
+
+}
